6603d9c03d956dfc55d69b007ac4b8ef81743477
Nitesh Hebbare <c0901466@mylambton.ca>
2024-10-18 18:15:52 -0400

    Assignment 3 Devops

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from urllib.parse import quote_plus
 from flask import Flask, render_template
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-username = "c0901466"  # replace with your actual username
-password = "G@9a*HH2XSYqCC6"  # replace with your actual password
+load_dotenv() #load env variables from .env files
 
-# # URL-encode the username and password
-encoded_username = quote_plus(username)
-encoded_password = quote_plus(password)
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+
+# URL-encode the username and password
+
+encoded_username = quote_plus(MONGODB_USERNAME)
+encoded_password = quote_plus(MONGODB_PASSWORD)
 
 # Connect to MongoDB Atlas
 client = MongoClient(f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.9bcht.mongodb.net/shop_db?retryWrites=true&w=majority&tls=true")
@@ -23,26 +28,10 @@ def home():
 
 @app.route('/products')
 def products():
-    print("Aagaya*****************************")
     products = products_collection.find()
-    # print("After Products="+str(len(products)))
-    # for i in products:
-    #     print(i)
     return render_template('products.html', products=products)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# from pymongo import MongoClient
-
-# # Replace <username>, <password>, and <dbname> with your values
-# client = MongoClient("mongodb+srv://encoded_username:encoded_password@cluster0.9bcht.mongodb.net/shop_db?retryWrites=true&w=majority&tls=true")
-
-# # Test connection
-# try:
-#     client.admin.command('ping')
-#     print("Connected successfully!")
-# except Exception as e:
-#     print("Could not connect to MongoDB:", e)
 
 
